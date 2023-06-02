d92fe216778514225c5644273a74422633224fac
Hugo Vanderlei <hugocvcosta@gmail.com>
2023-06-02 16:12:42 -0300

    update to only ios platform

--- Package.swift
+++ Package.swift
@@ -5,10 +5,7 @@ import PackageDescription
 let package = Package(
     name: "Resolver",
     platforms: [
-        .iOS(.v14),
-        .macOS(.v12),
-        .tvOS(.v13),
-        .watchOS(.v8)
+        .iOS(.v14)
     ],
     products: [
         .library(
